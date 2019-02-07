7e4c9d3d8ab364033f7c837f97a198d7f62be9db
doniyur <47398331+doniyur@users.noreply.github.com>
2019-02-07 16:46:29 +0800

    Add files via upload

---
+++ master.js
@@ -0,0 +1,6 @@
+var x=Math.floor((Math.random()*4)+ 1);
+var xxx=null;
+if(x=="1"){xxx="cause-symptoms-and-treatment.html"}
+if(x=="2"){xxx="the-3-stages-of-trading-blindness.html"}
+if(x=="3"){xxx="3-important-trading-tips-and-tricks.html"}
+if(x=="4"){xxx="what-makes-insurance-to-be-paradigm-for.html"}a_to_ve[i].href="https://arsip.guru-baik.com/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
